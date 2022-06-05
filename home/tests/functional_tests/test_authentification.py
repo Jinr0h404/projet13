51d833802d70faca97fa3451df4345c32fd227ed
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2022-06-05 12:18:35 +0200

    add service module for selenium

--- home/tests/functional_tests/test_authentification.py
+++ home/tests/functional_tests/test_authentification.py
@@ -2,6 +2,7 @@ import pytest
 
 from selenium import webdriver
 # from webdriver_manager.chrome import (ChromeDriverManager)
+from selenium.webdriver.chrome.service import Service
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth.models import User
 from django.urls import reverse

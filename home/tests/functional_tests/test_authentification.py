139ece8d777b2b0f5cbc4aaf6a4999bd7e2d8783
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2022-06-05 13:07:19 +0200

    change conf selenium use chromedriver in test authentfor chrome in circleCi yml conf

--- home/tests/functional_tests/test_authentification.py
+++ home/tests/functional_tests/test_authentification.py
@@ -1,26 +1,26 @@
 import pytest
 
 from selenium import webdriver
-# from webdriver_manager.chrome import (ChromeDriverManager)
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import (ChromeDriverManager)
+# from selenium.webdriver.chrome.service import Service
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
 from selenium.webdriver.common.by import By
-import os
+# import os
 
 
 class TestAuthentification(StaticLiveServerTestCase):
     @pytest.mark.django_db
     def test_signin(self):
         """functional test with selenium to verify the user signin scenario."""
-        os.chmod('home/tests/functional_tests/chromedriver', 755)
+        # os.chmod('home/tests/functional_tests/chromedriver', 755)
         username = "test_user"
         password = "Troubadour"
         User.objects.create_superuser(username=username, password=password)
-        self.s = Service(executable_path="home/tests/functional_tests/chromedriver")
-        self.browser = webdriver.Chrome(service=self.s)
-        # self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        # self.s = Service(executable_path="home/tests/functional_tests/chromedriver")
+        # self.browser = webdriver.Chrome(service=self.s)
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
         self.browser.get(self.live_server_url + reverse("home-signin"))
         login = self.browser.find_element(By.ID, "login")
         login.send_keys("test_user")

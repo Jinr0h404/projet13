1dc311e43142c73302d46ebad1958ad6aa8821b0
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2022-06-06 16:46:12 +0200

    git change setting to split dev and prod conf. Add dockerfile file to build app in docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9
+
+WORKDIR /app
+
+ENV PYTHONUNBUFFERED=1
+ENV PORT=8000
+
+ADD . /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+EXPOSE 8000
+
+CMD gunicorn apposteo.wsgi:application --bind 0.0.0.0:$PORT
--- apposteo/settings/__init__.py
+++ apposteo/settings/__init__.py
@@ -19,7 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +41,7 @@ DEFAULT_FROM_EMAIL = 'contact apposteo <contact@apposteo.com>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

307d9f9ac6d1743faaa84aaebd5a7cbb20b8cb19
ewen jeannenot <ewen.jeannenot@normandie.cci.fr>
2022-06-07 08:39:44 +0200

    clean directory from junk files and delete dev secretKey from github

--- apposteo/settings/__init__.py
+++ apposteo/settings/__init__.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-2*s%6k&62#r0mvwl-=4yvlv4q=i-kp4nq=p@u(!#4!&ntc0gm+')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 CSRF_TRUSTED_ORIGINS = ['https://apposteo.herokuapp.com']
 
